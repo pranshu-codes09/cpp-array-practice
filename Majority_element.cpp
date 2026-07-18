12b15f73fa856c02beaa2a9b45bea08211744ef4
pranshu-codes09 <pranshu7vachkal@gmail.com>
2026-07-18 17:04:06 +0530

    Create Majority_element.cpp

---
+++ Majority_element.cpp
@@ -0,0 +1,36 @@
+//Majority element in array
+
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[] = {1,2,2,3,2,2,5};
+    int size = sizeof(arr)/sizeof(int);
+    int candidate = 0;
+    int count = 0;
+    int printed[size] = {false};
+    for(int i = 0;i<size;i++){
+        if (count == 0){
+            candidate = arr[i];
+        }
+        if(candidate == arr[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+    count = 0;
+    for(int j = 0;j<size;j++){
+        if(arr[j]==candidate){
+            count++;
+        }
+        if(count>size/2){
+            cout<<"Maximum: "<<candidate<<endl;
+            cout<<"count: "<<count;
+            break;
+        }
+    }
+    
+   
+return 0;
+}
